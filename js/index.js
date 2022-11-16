77a960ea1155820fd3b80b197c4d96dfef9950c4
yousamasham <nazsemsem01@gmail.com>
2022-11-16 15:32:43 -0500

    Add homepage

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
 function loginClicked(){
     var username = document.getElementById("Username").value;
     var password = document.getElementById("Password").value;
+    var errorMessage = document.getElementById("loginError")
+    errorMessage.style.display = "none"
 
     if (username == "admin" && password == "admin"){
-        window.location.href = "nextpage.html"
+        window.location.href = "homepage.html"
     }
 
     else{
-        var errorMessage = document.getElementById("loginError")
         errorMessage.style.display = "block"
     }
 }
 
+function hasWhiteSpace(s) {
+    return s.indexOf(' ') >= 0;
+  }
+
 function registerClicked(){
-    window.location.href = "register.html   "
+    window.location.href = "register.html"
 }
+
+document.addEventListener('keypress', (event) => {
+    var name = event.key;
+
+    if (name == "Enter"){
+        document.getElementById("login").click()
+    }
+  }, false);

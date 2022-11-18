a8a84cab7c4f32d39d84518c910064fda0b2f42b
yousamasham <nazsemsem01@gmail.com>
2022-11-18 12:25:32 -0500

    finalize

--- js/register.js
+++ js/register.js
@@ -5,6 +5,11 @@ function registerClicked(){
     var email = document.getElementById("email")
     var ogPassword = document.getElementById("password")
     var secPassword = document.getElementById("password2")
+    fName.style.borderBlockColor = "unset"
+    lName.style.borderBlockColor = "unset"
+    email.style.borderBlockColor = "unset"
+    ogPassword.style.borderBlockColor = "unset"
+    secPassword.style.borderBlockColor = "unset"
     var re = new RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
     var fnError = document.getElementById("fnErrorMessage")
     var lnError = document.getElementById("lnErrorMessage")
@@ -28,35 +33,51 @@ function registerClicked(){
 
     //last name check
     if (lName.value == "" || hasWhiteSpace(lName.value)){
+        lName.style.borderBlockColor = "red"
         lnError.style.display = "block"
         return
     }
 
     //email checks
     if (!re.test(email.value)){
+        email.style.borderBlockColor = "red"
         eError.style.display = "block"
         return
     }
 
     //password check empty
     if (ogPassword.value == ""){
+        ogPassword.style.borderBlockColor = "red"
         epError.style.display = "block"
         return
     }
 
     //password check space
     if (hasWhiteSpace(ogPassword.value)){
+        ogPassword.style.borderBlockColor = "red"
         pwInvalidError.style.display = "block"
         return
     }
 
     //check if passwords match
     if (ogPassword.value != secPassword.value){
+        ogPassword.style.borderBlockColor = "red"
+        secPassword.style.borderBlockColor = "red"
         pwError.style.display = "block"
         return
     }
+
+    window.location.href = "homepage.html"
 }
 
 function hasWhiteSpace(s) {
     return s.indexOf(' ') >= 0;
   }
+
+document.addEventListener('keypress', (event) => {
+var name = event.key;
+
+if (name == "Enter"){
+    document.getElementById("login").click()
+    }
+}, false);

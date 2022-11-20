de439e1678c343977df0d75edf8627bf78229608
harunm1 <harun.mohammad2109@gmail.com>
2022-11-20 18:46:33 -0500

    Fixing file structure

--- js/tutorials.js
+++ js/tutorials.js
@@ -2,32 +2,32 @@
 let arms = [
     {
         name: "Bicep Curl",
-        front_vid: "./WorkoutVids/bicep-curl_front.mp4",
-        side_vid: "./WorkoutVids/bicep-curl_side.mp4",
+        front_vid: "./assets/vids/bicep-curl_front.mp4",
+        side_vid: "./assets/vids/bicep-curl_side.mp4",
         How: "Hold a dumbbell in each hand at arm's length. Use your biceps to curl the dumbbell until it reaches your shoulder, then lower them back down to your side and repeat.",
         Why: "Apart from building muscle size, boosting athletic performance and facilitating daily activities, bicep curls build strength in the upper arm, train shoulder to be more stable and teaches the practitioner's core to engage.",
         link: "https://www.youtube.com/watch?v=sAq_ocpRh_I&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=1"
     },
     {
         name: "Skull Crusher",
-        front_vid: "./WorkoutVids/skullcrusher_front.mp4",
-        side_vid: "./WorkoutVids/skullcrusher_side.mp4",
+        front_vid: "./assets/vids/skullcrusher_front.mp4",
+        side_vid: "./assets/vids/skullcrusher_side.mp4",
         How: "Lie back on a bench set to a 30-degree decline. Grip the EZ bar on the innermost grips and extend your arms straight up, palms facing your feet. Keeping your elbows fixed and tucked in, lower the bar until it is about an inch from your forehead. Slowly extend your arms back to the starting position without locking your elbows.",
         Why: " Don't be scared of the name, this is one of the arm exercises for ultimate triceps builders. Set the bench to decline and it'll increase your range of motion for increased muscle activation.",
         link: "https://www.youtube.com/watch?v=d_KZxkY_0cM&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=2&t=24s"
     },
     {
         name: "Chin Up", 
-        front_vid: "./WorkoutVids/chin-up_front.mp4",
-        side_vid: "./WorkoutVids/chin-up_side.mp4",
+        front_vid: "./assets/vids/chin-up_front.mp4",
+        side_vid: "./assets/vids/chin-up_side.mp4",
         How: "Grab the bar shoulder width apart with a supinated grip (palms facing you). With your body hanging and arms fully extended, pull yourself up until your chin is past the bar. Slowly return to starting position. Repeat.",
         Why: "The primary benefits of the chin-up are increasing strength and definition of the upper arms, specifically the biceps, the posterior deltoids of the shoulders and the teres major and latissimus dorsi muscles of the back.",
         link: "https://www.youtube.com/watch?v=bZ6Ysk9jf6E&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=3"
     },
     {
         name: "Wrist Curl",
-        front_vid: "./WorkoutVids/wrist-curl_front.mp4",
-        side_vid: "./WorkoutVids/wrist-curl_side.mp4",
+        front_vid: "./assets/vids/wrist-curl_front.mp4",
+        side_vid: "./assets/vids/wrist-curl_side.mp4",
         How: "Sit on a bench and hold a barbell with your palms facing up. Rest your forearms on your knees and lower the bar towards the floor as far as possible. Curl your wrist upward, pause, then slowly lower.",
         Why: "Your forearms will be on display long after your beach body has gone back into hiding, so give them due care. Your grip strength will be much improved come deadlift day too.",
         link: "https://www.youtube.com/watch?v=qMtmHwaCmYI&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=4",
@@ -36,32 +36,32 @@ let arms = [
 let legs = [
     {
         name: "Leg Extension",
-        front_vid: "./WorkoutVids/leg-extension_front.mp4",
-        side_vid: "./WorkoutVids/leg-extension_side.mp4",
+        front_vid: "./assets/vids/leg-extension_front.mp4",
+        side_vid: "./assets/vids/leg-extension_side.mp4",
         How: "Sit in a leg extension machine with your ankles against the lower pad. Use your quads to push forwards and straighten your legs in front of you, then return to the start position.",
         Why: "A great strengthening exercise for the quadriceps, which will again help with protecting the knees as we get older. Try using eccentric-focused reps: both legs perform the extension, one leg lowers the weight back.",
         link: "https://www.youtube.com/watch?v=YyvSfVjQeL0&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=5"
     },
     {
         name: "Leg Press",
-        front_vid: "./WorkoutVids/leg-press_front.mp4",
-        side_vid: "./WorkoutVids/leg-press_side.mp4", 
+        front_vid: "./assets/vids/leg-press_front.mp4",
+        side_vid: "./assets/vids/leg-press_side.mp4", 
         How: "Place your legs on the platform with your feet at shoulder width. Release the weight and extend your legs fully, without locking your knees. Lower the weight until your legs are at a 90° angle (but DO NOT allow your butt and lower back to rise off of the pad. This will put your lower back in a rounded position, which is very dangerous.) Raise the weight back to starting position.",
         Why: "The leg press develops and strengthens several muscles, such as the quadriceps, hamstring, and gluteal muscles. Since the calf muscles play a critical role in supporting these muscles, the calf muscles also become stronger.",
         link: "https://www.youtube.com/watch?v=oujca3_Shgw&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=6&t=39s"
     },
     {
         name: "Goblet Squat",
-        front_vid: "./WorkoutVids/goblet-squat_front.mp4",
-        side_vid: "./WorkoutVids/goblet-squat_side.mp4",
+        front_vid: "./assets/vids/goblet-squat_front.mp4",
+        side_vid: "./assets/vids/goblet-squat_side.mp4",
         How: "Stand with your legs wider than your shoulders and hold a dumbbell with both hands in line with your thighs. Stick your backside out, bend your knees and lower yourself into a squat until the dumbbell touches the floor. Drive back up and repeat.",
         Why: "You get the lower body muscle-building benefits reaped from traditional squats while torching your shoulders and arms at the same time. Goblet squats are a great alternative for those who lack the shoulder mobility to maintain proper back squat form.",
         link: "https://www.youtube.com/watch?v=MeIiIdhvXT4&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=7&t=21s"
     },
     {
         name: "Deadlift", 
-        front_vid: "./WorkoutVids/deadlift_front.mp4",
-        side_vid: "./WorkoutVids/deadlift_side.mp4",
+        front_vid: "./assets/vids/deadlift_front.mp4",
+        side_vid: "./assets/vids/deadlift_side.mp4",
         How: "Stand behind a grounded barbell. Bend your knees slightly to grab it, keeping your shins, back and hips straight. Without bending your back, push your hips forwards to lift the bar. From upright, push your hips back to lower the bar, bending your knees only slightly.",
         Why: "The traditional deadlift focuses on your glutes and superior end of your hamstrings. Practising hip flexion exercises can seriously improve your sprint speed and agility. After eight weeks of hip flexor-specific training, participants in a University of Florida study saw their sprint and shuttle run PBs(Personal Best) improve by around four per cent and nine per cent respectively.",
         link: "https://www.youtube.com/watch?v=7Q_GnXm7LbI&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=8&t=153s"
@@ -70,32 +70,32 @@ let legs = [
 let chest = [
     {
         name: "Incline Bench Press",
-        front_vid: "./WorkoutVids/incline-bench-press_front.mp4",
-        side_vid: "./WorkoutVids/incline-bench-press_side.mp4",
+        front_vid: "./assets/vids/incline-bench-press_front.mp4",
+        side_vid: "./assets/vids/incline-bench-press_side.mp4",
         How: "Lay on an incline bench in a rack, grip your barbell with arms outside of shoulder width and un-rack your bar. Slowly lower the barbell to the top of your chest under control, ensuring your elbows don’t flare too much. Pause here and press back up explosively before repeating.",
         Why: "Switching to an incline angle can help to target the upper portion of the chest, which may be underdeveloped if you’ve long favoured the flat bench press. This position also enables you to lower the barbell higher on your chest, creating a bigger stretch. You won’t be able to shift as much weight in this position, but what you do move will be more potent.",
         link: "https://www.youtube.com/watch?v=SrqOu55lrYU&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=9&t=36s"
     },
     {
         name: "Cable Crossover", 
-        front_vid: "./WorkoutVids/cable-crossover_front.mp4",
-        side_vid: "./WorkoutVids/cable-crossover_side.mp4",
+        front_vid: "./assets/vids/cable-crossover_front.mp4",
+        side_vid: "./assets/vids/cable-crossover_side.mp4",
         How: "Attach stirrup handles to the high pulleys of a cable crossover machine. Take one in each hand and your arms should be outstretched with a slight bend. Place one foot slightly forward, brace your core, and pull the handles downward and across your body. Return to the start position under control.",
         Why: "Cable crossovers target the pectoralis major muscles' sternal heads, found in the bottom of your chest as well as activating muscles in your shoulder and back. The standing Cable crossover exercise recruits the third most muscle fibres in your chest, only behind bench press and flyes.",
         link: "https://www.youtube.com/watch?v=WEM9FCIPlxQ&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=10"
     },
     {
         name: "Dip",
-        front_vid: "./WorkoutVids/dip_front.mp4",
-        side_vid: "./WorkoutVids/dip_side.mp4",
+        front_vid: "./assets/vids/dip_front.mp4",
+        side_vid: "./assets/vids/dip_side.mp4",
         How: "Jump up on two parallel bars or gymnastics rings with your palms facing inward and your arms straight — use two boxes or the backs of two sturdy chairs if you're at home. Bend your arms and slowly lower your body until your elbows are at right angles, ensuring they don't flare outward. Drive yourself back up to the top, squeezing your chest and arms hard, before repeating.",
         Why: "Unlike the humble press-up, dips require you to lift the entirety of your bodyweight from the ground. More resistance = more growth, even with bodyweight movements.",
         link: "https://www.youtube.com/watch?v=sM6XUdt1rm4&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=11&t=126s"
     },
     {
         name: "Push Up",
-        front_vid: "./WorkoutVids/push-up_front.mp4",
-        side_vid: "./WorkoutVids/push-up_side.mp4",
+        front_vid: "./assets/vids/push-up_front.mp4",
+        side_vid: "./assets/vids/push-up_side.mp4",
         How: "Place your hands firmly on the ground, directly under shoulders. Flatten your back so your entire body is straight and slowly lower your body. Draw shoulder blades back and down, keeping elbows tucked close to your body. Exhale as you push back to the starting position.",
         Why: "Traditional pushups are beneficial for building upper body strength. They work the triceps, pectoral muscles, and shoulders. When done with proper form, they can also strengthen the lower back and core by engaging (pulling in) the abdominal muscles. Pushups are a fast and effective exercise for building strength.",
         link: "https://www.youtube.com/watch?v=wxhNoKZlfY8&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=12&t=14s"
@@ -104,32 +104,32 @@ let chest = [
 let abs = [
     {
         name: "Sit Up",
-        front_vid: "./WorkoutVids/situp_front.mp4", 
-        side_vid: "./WorkoutVids/situp_side.mp4",
+        front_vid: "./assets/vids/situp_front.mp4", 
+        side_vid: "./assets/vids/situp_side.mp4",
         How: "Lie down on the floor with your knees bent and, if possible, hook your feet under something that will prevent them from moving. Place your hands behind your head and tense your core as you lift your torso up so your upper body forms a V shape with your thighs. Lower under control back to the start position.",
         Why: "They promote good posture by working your lower back and gluteal muscles. With a larger range of motion, situps target more muscles than crunches and static core exercises. This makes them an ideal addition to your fitness program.",
         link: "https://www.youtube.com/watch?v=NGRKFMKhF8s&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=13&t=54s"
     },
     {
         name: "Laying Leg Raise",
-        front_vid: "./WorkoutVids/leg-raise_front.mp4",
-        side_vid: "./WorkoutVids/leg-raise_side.mp4",
+        front_vid: "./assets/vids/leg-raise_front.mp4",
+        side_vid: "./assets/vids/leg-raise_side.mp4",
         How: "Lay on your back with your arms palms down on either side. Keep your legs together and as straight as possible. Slowly raise your legs to a 90° angle, pause at this position, or as high as you can reach your legs, and then slowly lower your legs back down. Duration of these movements should be slow so that you do not utilize momentum, enabling you to get the most out of the exercise.",
         Why: "It does need a pull-up bar to work, but can still be done in the comfort of your own home. Variations such as the hanging twisting knee raise keeps the exercise fresh.",
         link: "https://www.youtube.com/watch?v=l4kQd9eWclE&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=14&t=7s"
     },
     {
         name: "Russian Twist",
-        front_vid: "./WorkoutVids/russian-twist_front.mp4",
-        side_vid: "./WorkoutVids/russian-twist_side.mp4",
+        front_vid: "./assets/vids/russian-twist_front.mp4",
+        side_vid: "./assets/vids/russian-twist_side.mp4",
         How: "Sit holding the dumbell with your arms extended and feet off the floor. Quickly twist at the torso, turning from side to side.",
         Why: "The only other exercise on the list that requires any equipment. Much more effective than static rotations standing up, your core must be continually engaged to keep you balanced while the twist works either side of your body.",
         link: "https://www.youtube.com/watch?v=wkD8rjkodUI&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=15&t=5s"
     },
     {
         name: "Barbell Rollouts", 
-        front_vid: "./WorkoutVids/barbell-rollout_front.mp4",
-        side_vid: "./WorkoutVids/barbell-rollout_side.mp4",
+        front_vid: "./assets/vids/barbell-rollout_front.mp4",
+        side_vid: "./assets/vids/barbell-rollout_side.mp4",
         How: "Load a barbell with weights if needed and grab the bar with an overhand, shoulder-width grip. Position your shoulders directly over the barbell and slowly roll the bar forwards. Pause, then reverse the move. Roll out to a distance that's challenging, but doesn't force your hips to sag.",
         Why: "Barbell rollouts are extremely effective in working on core strength and stability. You go from being unable to do a full rep to easily doing multiple. The movement effectively engages every muscle group of the core. It improves coordination, lumbar positioning, and mobility.",
         link: "https://www.youtube.com/watch?v=mIYuVpQF3N8&list=PLOytLgEoo4wemK6fbf1eBC1beH4qjCIra&index=16&t=94s"

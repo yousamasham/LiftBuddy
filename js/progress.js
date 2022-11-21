a03129ab18993f7707ce62e7bf07a9545dff7476
Rahul A. Sundarrajan <rahul.sundarrajan@gmail.com>
2022-11-21 04:51:08 -0500

    Inserted gifs. Abandoned dynamic images.

--- js/progress.js
+++ js/progress.js
@@ -1,18 +1,15 @@
 const submit = document.querySelector(".enter");
-const imgList = document.querySelector(".images-list");
 
-let metric = document.getElementById("metric");
-let selection = metric.options[metric.selectedIndex].text;
+let check = document.getElementById('enter');
+if (check) {
+    submit.addEventListener("click", imageAdd());
+}
 
-let date = document.getElementById("metric");
-date.max = new Date().toISOString().split("T")[0];
-let dateRegex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
-
-submit.addEventListener("click", function () {
-    if (selection === "bench press: weight" && dateRegex.test(date)) 
-    {
-        $('#imagesList').append('<li><img src="assets/img/weight_plot.png"/></li>');
-    }
-} )
-
-
+function imageAdd() {
+    var img = document.createElement("img");
+    img.src = 'assets/img/weight_plot.png';
+    img.width = auto;
+    img.height = auto;
+    img.alt = 'Weight versus Time Plot';
+    document.body.appendChild(img);
+}
